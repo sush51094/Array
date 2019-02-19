73db588897ac7c8a31778bf765f6ebd1e73f1010
MacStudent <macstudent@126-iMac12.local>
2019-02-19 17:37:44 -0500

    Initial Commit

---
+++ Array/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Array
+//
+//  Created by MacStudent on 2019-02-19.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
