70b1675e8be5cf3a72c6a44d38ec573535ecfd6d
MacStudent <macstudent@126-iMac12.local>
2019-02-19 18:43:21 -0500

    initial commit

--- Array/main.swift
+++ Array/main.swift
@@ -10,3 +10,25 @@ import Foundation
 
 print("Hello, World!")
 
+let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+
+print(names)
+
+func backward(_ s1: String, _ s2 : String) -> Bool
+{
+    return s1 > s2
+    
+}
+
+func forward(_ s1: String, _ s2 : String) -> Bool
+{
+    return s1 < s2
+}
+var reversedNames1 = names.sorted(by: backward)
+
+print(reversedNames1)
+
+var reversedNames2 = names.sorted(by: forward)
+
+print(reversedNames2)
+
